8cca7d1af8494945672deaa343a7e405320204b8
Cesar Cerda <cesarcesar2812@outlook.com>
2025-08-28 19:57:46 -0600

    Mejorando la Herencia

--- LayerDomainModel/DMPersona.cs
+++ LayerDomainModel/DMPersona.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace LayerDomainModel;
 
-public interface DMPersona : DMPersonaNatural, DMPersonaJuridica
+public class DMPersona : DMPersonaNatural, DMPersonaJuridica
 {
     public int IdPersona { get; set; }
     public string? NombrePersona { get; set; }
@@ -9,5 +9,25 @@ public interface DMPersona : DMPersonaNatural, DMPersonaJuridica
     public int Id_municipo { get; set; }
 
     public DMMunicipio? objMunicipio { get; set; }
+
+
+
+    //persona Juridica
+    public int IdPersonaJuridica { get; set; }
+
+    public string? NumeroRuc { get; set; }
+
+    public string? RazonSocial { get; set; }
+
+
+    //persona Natural
+    public int IdPersonaNatural { get; set; }
+    public string? CedulaPerosonaNatural { get; set; }
+
+    public string? ApellidoPersonaNatural { get; set; }
+
+    public int Id_genero { get; set; }
+
+    public DMGenero? objGenero { get; set; }
 
 }
