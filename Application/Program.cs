a3b21f0a8c2379ebc8c7336ae157c20587aea68f
Cesar Cerda <cesarcesar2812@outlook.com>
2025-09-07 23:02:53 -0600

    Realizamos inyeccion de depencias

--- Application/Program.cs
+++ Application/Program.cs
@@ -24,7 +24,7 @@ builder.Configuration
 
 
 builder.Services.Configure<Conection>(builder.Configuration.GetSection("ConnectionStrings"));//inyeccion de dependencia a la base de datos
-builder.Services.Configure<Ruta>(builder.Configuration.GetSection("Configuracion"));//inyecccion de depencias al servidor de imagenes o carpeta para almacenar imagenes
+builder.Services.Configure<Ruta>(builder.Configuration.GetSection("Configuracion"));//inyecccion de depencias al servidor de imagenes o carpeta para almacenar imagenes y archivos
 
 //Inyeccion de depencias inyectando las clases que van a implementar una interface del caso de uso de Usuarios
 //casos de uso de usuarios
